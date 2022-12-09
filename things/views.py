066eabb3db05dc0b8c10274cb59d70ae585217b7
Vaibhavkumar Patel <Vaibhavkumar.patel@kcl.ac.uk>
2022-12-09 15:04:18 +0000

    Add the project things

--- things/views.py
+++ things/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
-from django.shortcuts import redirect
+
+# Create your views here.
 def home(request):
-    return render(request, 'things.html')
+    return render(request, 'thing.html')
