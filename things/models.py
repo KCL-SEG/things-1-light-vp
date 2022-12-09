fb81571a4bd1cd0784c5fede2e000bc5c25ec3cb
Vaibhavkumar Patel <Vaibhavkumar.patel@kcl.ac.uk>
2022-12-09 15:13:57 +0000

    new Thing model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Thing(models.Model):
+    name = models.CharField(unique=True, blank=False, max_length=30)
+    description = models.TextField(unique=False, blank=True ,max_length=120)
+    quantity = models.IntegerField(unique=False)
+    
+    
